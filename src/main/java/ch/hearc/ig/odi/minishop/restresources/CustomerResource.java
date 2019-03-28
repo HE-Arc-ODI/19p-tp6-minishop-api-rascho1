3180ecd1eac4f8ad6d256d15099d52ccc11e6ba1
rascho1 <raphael.schoeni@he-arc.ch>
2019-03-28 20:55:49 +0100

    Dans CustomerResource compléter et reformatage du code de la class

--- src/main/java/ch/hearc/ig/odi/minishop/restresources/CustomerResource.java
+++ src/main/java/ch/hearc/ig/odi/minishop/restresources/CustomerResource.java
@@ -8,13 +8,18 @@ import ch.hearc.ig.odi.minishop.business.Customer;
 import ch.hearc.ig.odi.minishop.services.PersistenceService;
 import java.util.List;
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 @Path("customer")
-
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 
 public class CustomerResource {
+
   @Inject
   private PersistenceService persistenceService;
 
