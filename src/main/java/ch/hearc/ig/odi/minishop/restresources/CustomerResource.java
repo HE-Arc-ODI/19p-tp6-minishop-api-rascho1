7f74511018bb82c574eb9fdf275b413073b9439d
rascho1 <raphael.schoeni@he-arc.ch>
2019-03-31 15:15:07 +0200

    Modification PersistanceService ajouter méthode updateCustomer

--- src/main/java/ch/hearc/ig/odi/minishop/restresources/CustomerResource.java
+++ src/main/java/ch/hearc/ig/odi/minishop/restresources/CustomerResource.java
@@ -15,6 +15,7 @@ import javax.ws.rs.DELETE;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -58,6 +59,18 @@ public class CustomerResource {
     } catch (CustomerException e) {
       e.printStackTrace();
       throw new NullFormException("customer not deleted.");
+    }
+  }
+
+  @PUT
+  @Path("{id}")
+  @Consumes(MediaType.APPLICATION_JSON)
+  public Customer updateCustomer(@PathParam("id") Long id, Customer customer) {
+    try {
+      return persistenceService.updateCustomer(id, customer);
+    } catch (CustomerException e) {
+      e.printStackTrace();
+      throw new NullFormException("product couldn't have been updated.");
     }
   }
 
